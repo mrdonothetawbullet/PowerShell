58c344fefac49321541dbc59de3382c368fcfc0f
Muhammad Suhail <suhail.asrul@gmail.com>
2021-02-09 11:02:15 +0800

    Update temp.ps1

--- temp.ps1
+++ temp.ps1
@@ -1,13 +1,12 @@
-$file1 = 'C:\Users\eric\Documents\Book1.xlsx' # source's fullpath
-$file2 = 'C:\Users\eric\Documents\Book2.xlsx' # destination's fullpath
-$xl = new-object -c excel.application
-$xl.displayAlerts = $false # don't prompt the user
-$wb2 = $xl.workbooks.open($file1, $null, $true) # open source, readonly
-$wb1 = $xl.workbooks.open($file2) # open target
-$sh1_wb1 = $wb1.sheets.item(2) # second sheet in destination workbook
-$sheetToCopy = $wb2.sheets.item('Sheet3') # source sheet to copy
-$sheetToCopy.copy($sh1_wb1) # copy source sheet to destination workbook
-$wb2.close($false) # close source workbook w/o saving
-$wb1.close($true) # close and save destination workbook
-$xl.quit()
-spps -n excel
+$File1 = "$ScriptDir\MasterPatchingList_v0.9.xlsx"
+$File2 = "$ScriptDir\Temp_Batch3.xlsx"
+$xl = new-object -c excel.application 
+$xl.displayAlerts = $false  # don't prompt the user
+$wb2 = $xl.workbooks.open($file1, $null, $true)  # open source, readonly
+$wb1 = $xl.workbooks.open($file2)  # open target
+$sh1_wb1 = $wb1.sheets.item(1)  # second sheet in destination workbook
+$sheetToCopy = $wb2.sheets.item('PatchingList')  # source sheet to copy
+$sheetToCopy.copy($sh1_wb1)  # copy source sheet to destination workbook
+$wb2.close($false)  # close source workbook w/o saving
+$wb1.close($true)  # close and save destination workbook
+$xl.quit() 
