6081d8e3c668acf3d5651e999442fcfc798d393a
Muhammad Suhail <suhail.asrul@gmail.com>
2021-02-11 16:57:39 +0800

    Update temp.ps1

--- temp.ps1
+++ temp.ps1
@@ -1,14 +1,12 @@
-$xl = New-Object -COM "Excel.Application"
-$xl.Visible = $true
-
-$wb = $xl.Workbooks.Open("C:\path\to\your.xlsx")
-$ws = $wb.Sheets.Item(1)
-
-$rows = $ws.UsedRange.Rows.Count
-
-foreach ( $col in "A", "B", "C" ) {
-  $xl.WorksheetFunction.CountIf($ws.Range($col + "1:" + $col + $rows), "<>") - 1
-}
-
-$wb.Close()
-$xl.Quit()
+$file1 = 'C:\Users\eric\Documents\Book1.xlsx' # source's fullpath
+$file2 = 'C:\Users\eric\Documents\Book2.xlsx' # destination's fullpath
+$xl = new-object -c excel.application
+$xl.displayAlerts = $false # don't prompt the user
+$wb2 = $xl.workbooks.open($file1, $null, $true) # open source, readonly
+$wb1 = $xl.workbooks.open($file2) # open target
+$sh1_wb1 = $wb1.sheets.item(2) # second sheet in destination workbook
+$sheetToCopy = $wb2.sheets.item('Sheet3') # source sheet to copy
+$sheetToCopy.copy($sh1_wb1) # copy source sheet to destination workbook
+$wb2.close($false) # close source workbook w/o saving
+$wb1.close($true) # close and save destination workbook
+$xl.quit()
