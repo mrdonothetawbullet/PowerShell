e4200800ab194c32b502daaf99b55658a06753b1
Muhammad Suhail <suhail.asrul@gmail.com>
2021-02-11 09:08:30 +0800

    Update temp.ps1

--- temp.ps1
+++ temp.ps1
@@ -1,1 +1,14 @@
-Start-Process -FilePath 'C:\Program Files\Microsoft Office\root\Office16\excelcnv.exe' -ArgumentList "-nme -oice ""$xlsFilePath"" ""$xlsToxlsxPath"""
+$xl = New-Object -COM "Excel.Application"
+$xl.Visible = $true
+
+$wb = $xl.Workbooks.Open("C:\path\to\your.xlsx")
+$ws = $wb.Sheets.Item(1)
+
+$rows = $ws.UsedRange.Rows.Count
+
+foreach ( $col in "A", "B", "C" ) {
+  $xl.WorksheetFunction.CountIf($ws.Range($col + "1:" + $col + $rows), "<>") - 1
+}
+
+$wb.Close()
+$xl.Quit()
