3825fb05971a56a8f484ff5c6ecb06ae2e15a2b2
Muhammad Suhail <suhail.asrul@gmail.com>
2021-02-26 19:54:58 +0800

    Delete MyVariable.ps1

--- MyVariable.ps1
+++
@@ -1,9 +0,0 @@
-﻿Clear-Host
-Get-PSSession | Remove-PSSession
-Remove-Variable * -ErrorAction SilentlyContinue; $Error.Clear();
-$Password = ConvertTo-SecureString "Welcome11" -AsPlainText -Force
-$Cred = New-Object System.Management.Automation.PSCredential ("globalnet\suhail_asrulsani-ops", $Password)
-$ScriptDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
-$Machinelist = @(get-content -Path "$ScriptDir\Machinelist.txt")
-$datetime = Get-Date -Format G
-$dt = (Get-Date).ToString("ddMMyyyy_HHmmss")
