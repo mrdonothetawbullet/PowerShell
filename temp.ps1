d5381b6e6ceb5baa9b649193520c583b68cebee9
Muhammad Suhail <suhail.asrul@gmail.com>
2021-03-03 14:25:23 +0800

    Update temp.ps1

--- temp.ps1
+++ temp.ps1
@@ -1,12 +1,1 @@
-[cmdletbinding()]
-Param([string]$computer=$env:computername)
-
-$query="Associators of {Win32_Group.Domain='$computer',Name='Administrators'} where Role=GroupComponent"
-
-write-verbose "Querying $computer"
-write-verbose $query 
-
-Get-CIMInstance -query $query -computer $computer | 
-Select @{Name="Member";Expression={$_.Caption}},Disabled,LocalAccount,
-@{Name="Type";Expression={([regex]"User|Group").matches($_.Class)[0].Value}},
-@{Name="Computername";Expression={$_.ComputerName.ToUpper()}}
+Get-ExchangeServer | Format-List Name, Edition, AdminDisplayVersion
